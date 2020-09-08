22fd74061a92a49089d90fbaeab0e8831a8b64e3
Soup-64 <43444191+Soup-64@users.noreply.github.com>
2020-09-08 18:25:51 -0400

    created main folder and a single practice project
    +main folder
    +one project that serves as practice and as a template

---
+++ APCS_projects/music player/music player/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace music_player
+{
+    partial class Music
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Song_box = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Song_box
+            // 
+            this.Song_box.FormattingEnabled = true;
+            this.Song_box.ItemHeight = 15;
+            this.Song_box.Location = new System.Drawing.Point(12, 12);
+            this.Song_box.Name = "Song_box";
+            this.Song_box.Size = new System.Drawing.Size(277, 454);
+            this.Song_box.TabIndex = 0;
+            this.Song_box.Tag = "cum";
+            this.Song_box.DoubleClick += new System.EventHandler(this.Songs_DoubleClick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(295, 397);
+            this.trackBar1.MinimumSize = new System.Drawing.Size(500, 0);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(500, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 427);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(757, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Music
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(836, 471);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Song_box);
+            this.MinimumSize = new System.Drawing.Size(500, 0);
+            this.Name = "Music";
+            this.Text = "Music";
+            this.Load += new System.EventHandler(this.Music_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Song_box;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ APCS_projects/music player/music player/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace music_player
+{
+    public partial class Music : Form
+    {
+        private String[] Song_list = (string[])Directory.EnumerateFiles(@"C:\Users\Owner\Desktop\Music_folder");
+        private int last_sel = -1;
+
+        public Music()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Music_Load(object sender, EventArgs e)
+        {
+
+            Song_box.BeginUpdate();
+            for(int i = 0; i < Song_list.Length; i++)
+            {
+                Song_box.Items.Add(Song_list[i]);
+            }
+            Song_box.EndUpdate();
+        }
+
+        private void Songs_DoubleClick(object sender, EventArgs e)
+        {
+            //remove this, it is test code
+            ListBox lst = (ListBox)sender;
+            
+
+            if(lst.SelectedIndex != last_sel)
+            {
+                
+                Console.WriteLine(e.GetType() + "\n" + lst.SelectedIndex + "\n" + lst.Text);
+                last_sel = lst.SelectedIndex;
+            }
+        }
+    }
+}
