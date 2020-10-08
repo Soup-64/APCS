ac18ec42539e065c49391a120972e15a71971040
Bm-64 <bmanbros@gmail.com>
2020-10-08 13:17:37 -0400

    finishing touches
    +cleaned up code
    +other stuff

--- APCS_projects/music player/music player/Player.Designer.cs
+++ APCS_projects/music player/music player/Player.Designer.cs
@@ -41,6 +41,10 @@
             this.crnt_Song = new System.Windows.Forms.TextBox();
             this.Add_playlist = new System.Windows.Forms.Button();
             this.Remove_playlist = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.Rename_playlist = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.name_input = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.seeker)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.song_img)).BeginInit();
             this.SuspendLayout();
@@ -50,14 +54,14 @@
             this.Song_box.AllowDrop = true;
             this.Song_box.FormattingEnabled = true;
             this.Song_box.ItemHeight = 15;
-            this.Song_box.Location = new System.Drawing.Point(10, 34);
+            this.Song_box.Location = new System.Drawing.Point(10, 31);
             this.Song_box.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Song_box.Name = "Song_box";
-            this.Song_box.Size = new System.Drawing.Size(277, 454);
+            this.Song_box.Size = new System.Drawing.Size(277, 499);
             this.Song_box.TabIndex = 0;
             this.Song_box.DragDrop += new System.Windows.Forms.DragEventHandler(this.Song_box_drag_drop);
             this.Song_box.DragEnter += new System.Windows.Forms.DragEventHandler(this.Song_box_drag_enter);
-            this.Song_box.DoubleClick += new System.EventHandler(this.Songs_DoubleClick);
+            this.Song_box.DoubleClick += new System.EventHandler(this.List_DoubleClick);
             // 
             // seeker
             // 
@@ -94,7 +98,7 @@
             // song_img
             // 
             this.song_img.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.song_img.Location = new System.Drawing.Point(380, 12);
+            this.song_img.Location = new System.Drawing.Point(380, 32);
             this.song_img.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.song_img.Name = "song_img";
             this.song_img.Size = new System.Drawing.Size(350, 346);
@@ -111,7 +115,7 @@
             this.play_Btn.TabIndex = 5;
             this.play_Btn.Text = "Play";
             this.play_Btn.UseVisualStyleBackColor = true;
-            this.play_Btn.Click += new System.EventHandler(this.Play_Click);
+            this.play_Btn.Click += new System.EventHandler(this.Play_Song);
             // 
             // pause_Btn
             // 
@@ -122,7 +126,7 @@
             this.pause_Btn.TabIndex = 5;
             this.pause_Btn.Text = "Pause";
             this.pause_Btn.UseVisualStyleBackColor = true;
-            this.pause_Btn.Click += new System.EventHandler(this.Pause_Click);
+            this.pause_Btn.Click += new System.EventHandler(this.Pause_Song);
             // 
             // forward
             // 
@@ -133,7 +137,7 @@
             this.forward.TabIndex = 5;
             this.forward.Text = "Forward";
             this.forward.UseVisualStyleBackColor = true;
-            this.forward.Click += new System.EventHandler(this.Forward_Click);
+            this.forward.Click += new System.EventHandler(this.Skip_backward);
             // 
             // backward
             // 
@@ -144,7 +148,7 @@
             this.backward.TabIndex = 5;
             this.backward.Text = "Backward";
             this.backward.UseVisualStyleBackColor = true;
-            this.backward.Click += new System.EventHandler(this.Backward_Click);
+            this.backward.Click += new System.EventHandler(this.Skip_forward);
             // 
             // crnt_Song
             // 
@@ -157,21 +161,51 @@
             // 
             // Add_playlist
             // 
-            this.Add_playlist.Location = new System.Drawing.Point(10, 5);
+            this.Add_playlist.Location = new System.Drawing.Point(10, 2);
             this.Add_playlist.Name = "Add_playlist";
             this.Add_playlist.Size = new System.Drawing.Size(75, 23);
             this.Add_playlist.TabIndex = 7;
             this.Add_playlist.Text = "Add";
             this.Add_playlist.UseVisualStyleBackColor = true;
+            this.Add_playlist.Click += new System.EventHandler(this.Create_playlist);
             // 
             // Remove_playlist
             // 
-            this.Remove_playlist.Location = new System.Drawing.Point(91, 5);
+            this.Remove_playlist.Location = new System.Drawing.Point(172, 2);
             this.Remove_playlist.Name = "Remove_playlist";
             this.Remove_playlist.Size = new System.Drawing.Size(75, 23);
             this.Remove_playlist.TabIndex = 8;
             this.Remove_playlist.Text = "Remove";
             this.Remove_playlist.UseVisualStyleBackColor = true;
+            this.Remove_playlist.Click += new System.EventHandler(this.Delete_playlist);
+            // 
+            // Back
+            // 
+            this.Back.Enabled = false;
+            this.Back.Location = new System.Drawing.Point(91, 2);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 9;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Rename_playlist
+            // 
+            this.Rename_playlist.Location = new System.Drawing.Point(253, 2);
+            this.Rename_playlist.Name = "Rename_playlist";
+            this.Rename_playlist.Size = new System.Drawing.Size(75, 23);
+            this.Rename_playlist.TabIndex = 10;
+            this.Rename_playlist.Text = "Rename";
+            this.Rename_playlist.UseVisualStyleBackColor = true;
+            this.Rename_playlist.Click += new System.EventHandler(this.Rename_Click);
+            // 
+            // name_input
+            // 
+            this.name_input.Location = new System.Drawing.Point(334, 3);
+            this.name_input.Name = "name_input";
+            this.name_input.Size = new System.Drawing.Size(196, 23);
+            this.name_input.TabIndex = 11;
             // 
             // Music
             // 
@@ -179,7 +213,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(851, 500);
+            this.ClientSize = new System.Drawing.Size(851, 532);
+            this.Controls.Add(this.name_input);
+            this.Controls.Add(this.Rename_playlist);
+            this.Controls.Add(this.Back);
             this.Controls.Add(this.Remove_playlist);
             this.Controls.Add(this.Add_playlist);
             this.Controls.Add(this.crnt_Song);
@@ -197,7 +234,6 @@
             this.MinimumSize = new System.Drawing.Size(500, 39);
             this.Name = "Music";
             this.Text = "Music Player";
-            this.Load += new System.EventHandler(this.Music_Load);
             ((System.ComponentModel.ISupportInitialize)(this.seeker)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.song_img)).EndInit();
             this.ResumeLayout(false);
@@ -219,6 +255,10 @@
         private System.Windows.Forms.TextBox crnt_Song;
         private System.Windows.Forms.Button Add_playlist;
         private System.Windows.Forms.Button Remove_playlist;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Rename_playlist;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox name_input;
     }
 }
 
--- APCS_projects/music player/music player/Player.cs
+++ APCS_projects/music player/music player/Player.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
-using NAudio.Wave;
+
 
 namespace music_player
 {
@@ -11,39 +12,38 @@ namespace music_player
     {
         private string[] Song_list;
 
-        private AudioFileReader Reader;
-        private WaveOutEvent Wave_out = new WaveOutEvent();
-        private int Current_index = -1;
-        private System.Timers.Timer Timer = new System.Timers.Timer();
-        private bool playlist_mode;
-        private string current_playlist;
+        private AudioFileReader Reader; //reads file data
+        private readonly WaveOutEvent Wave_out = new WaveOutEvent(); //plays audio
+
+        private int Current_index = -1; //current song being played
+
+        private readonly System.Timers.Timer Timer = new System.Timers.Timer(); //used to trigger the updates for the ui
+        private string current_playlist; //playlist in use, needed for the drag and drop
 
 
         //initialization methods
 
-        public Music()
+        public Music() //constructor
         {
-            Timer.Interval = 1000;
-            Timer.Elapsed += new System.Timers.ElapsedEventHandler(Update_time_seeker);
-            Wave_out.PlaybackStopped += new System.EventHandler<NAudio.Wave.StoppedEventArgs>(Song_ended);
+
 
             InitializeComponent();
-        }
 
-        private void Music_Load(object sender, EventArgs e) //fills the list when the form starts
-        {
             if (!Directory.Exists("C:/temp/playlists"))
             {
                 Directory.CreateDirectory("C:/temp/playlists");
             }
 
+            Timer.Interval = 1000;
+            Timer.Elapsed += new System.Timers.ElapsedEventHandler(Update_time_seeker);
+            Wave_out.PlaybackStopped += new System.EventHandler<NAudio.Wave.StoppedEventArgs>(Song_ended);
+
             Show_playlists();
 
             song_img.Image = Properties.Resources.no_cover;
-
         }
 
-        private void Init_list(string path)
+        private void Init_list(string path) //inits Song_list
         {
             if (File.Exists(path))
             {
@@ -52,19 +52,16 @@ namespace music_player
 
                 for (int i = 0; i < contents.Length - 1; i++)//intentional since last entry is empty bc of the \n 
                 {
-                    Console.Write(contents[i]);
-                    Console.Write("a");
                     foreach (string item in Directory.GetFiles(contents[i], "*.mp3*", SearchOption.AllDirectories))
                     {
                         directory_builder.Add(item);
-                        Console.WriteLine(item);
                     }
                 }
                 Song_list = directory_builder.ToArray();
             }
         }
-
-        private void Update_list_box()
+      
+        private void Update_song_list() //refreshes the list of songs
         {
             if (Song_list != null)
             {
@@ -93,42 +90,42 @@ namespace music_player
 
         //button/interface events
 
-        private void Songs_DoubleClick(object sender, EventArgs e) //new song selected
+        private void List_DoubleClick(object sender, EventArgs e) //play selected song
         {
-            if (playlist_mode)
+            if (Song_list[Song_box.SelectedIndex].EndsWith(".txt"))
             {
                 current_playlist = Directory.GetFiles("C:/temp/playlists")[Song_box.SelectedIndex];
 
                 Init_list(current_playlist);
 
-                Update_list_box();
+                Update_song_list();
 
-                playlist_mode = false;
+                Add_playlist.Enabled = false;
+                Rename_playlist.Enabled = false;
+                Remove_playlist.Enabled = false;
+                Back.Enabled = true;
             }
             else
             {
-                if (Song_box.SelectedIndex != Current_index)
+
+                Current_index = Song_box.SelectedIndex;
+
+                if (Reader != null)
                 {
-
-                    Console.WriteLine(Song_box.Text);
-                    Current_index = Song_box.SelectedIndex;
-
-                    if (Reader != null)
-                    {
-                        End_audio();
-                        Start_audio(Current_index);
-                    }
-                    else
-                    {
-                        Start_audio(Current_index);
-                    }
-
-                    Update_playing();
+                    End_audio();
+                    Start_audio(Current_index);
                 }
+                else
+                {
+                    Start_audio(Current_index);
+                }
+
+                Update_playing();
+
             }
         }
 
-        private void Play_Click(object sender, EventArgs e)
+        private void Play_Song(object sender, EventArgs e)//play song if paused
         {
             if (Reader != null)
             {
@@ -137,7 +134,7 @@ namespace music_player
             }
         }
 
-        private void Pause_Click(object sender, EventArgs e)
+        private void Pause_Song(object sender, EventArgs e)//pause song
         {
             if (Reader != null)
             {
@@ -145,57 +142,65 @@ namespace music_player
             }
         }
 
-        private void Backward_Click(object sender, EventArgs e)
+        private void Skip_forward(object sender, EventArgs e)//skip backward one song
         {
-            End_audio();
+            if (Reader != null)
+            {
+                End_audio();
 
-            Current_index = Math.Clamp(Current_index - 1, 0, Song_list.Length);
-            Start_audio(Current_index);
+                Current_index = Math.Clamp(Current_index - 1, 0, Song_list.Length);
+                Start_audio(Current_index);
 
-            Update_playing();
+                Update_playing();
+            }
         }
 
-        private void Forward_Click(object sender, EventArgs e)
+        private void Skip_backward(object sender, EventArgs e)//skip forward one song
         {
-            End_audio();
-            Current_index = Math.Clamp(Current_index + 1, 0, Song_list.Length);
-            Start_audio(Current_index);
+            if (Reader != null)
+            {
+                End_audio();
+                Current_index = Math.Clamp(Current_index + 1, 0, Song_list.Length);
+                Start_audio(Current_index);
 
-            Update_playing();
+                Update_playing();
+            }
         }
 
         private void Move_seeker(object sender, EventArgs e) //skips to the position that matches the seeker
         {
-            if (Reader != null && seeker.Value != (int)Reader.CurrentTime.TotalSeconds)
+            if (Reader != null && seeker.Value != (int)Reader.CurrentTime.TotalSeconds) 
+                //if seeker does not math position (can only desync if user moves it)
             {
-                Console.WriteLine(seeker.Value);
                 Reader.Skip(seeker.Value - (int)Reader.CurrentTime.TotalSeconds);
             }
         }
 
-        private void Song_box_drag_drop(object sender, DragEventArgs e)//gets directories or files
+        private void Song_box_drag_drop(object sender, DragEventArgs e)//adds dropped folder directories to current playlist
         {
             string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
 
             string contents;
 
-            foreach (string file in files)
+            foreach (string folder_dir in files)
             {
-                if (File.GetAttributes(file).HasFlag(FileAttributes.Directory))//if is folder
-                {
-                    contents = File.ReadAllText(current_playlist);
 
-                    if (!contents.Contains(file.Replace('\\', '/')))
+                if (File.GetAttributes(folder_dir).HasFlag(FileAttributes.Directory))//if is folder
+                {
+                    contents = File.ReadAllText(current_playlist); 
+                    //gets all dirs in current playlist
+
+                    if (!contents.Contains(folder_dir.Replace('\\', '/'))) 
+                        //if current playlist does not already contain the same directory
                     {
 
-                        File.AppendAllText(current_playlist, file.Replace('\\', '/') + "\n"); //duplicate check
-                                                                                                 //method also accounts for the file not existing
-
+                        File.AppendAllText(current_playlist, folder_dir.Replace('\\', '/') + "\n"); //writes directory path to playlist
+                                                                                              //method also accounts for the file not existing
                         Init_list(current_playlist);
 
-                        Update_list_box();
+                        Update_song_list();
                     }
-
+                    //note: the replace() thing fixes the slashes in the directory so C# reads it correctly
                 }
             }
         }
@@ -207,16 +212,71 @@ namespace music_player
                 e.Effect = DragDropEffects.Link;
             }
         }
-       
+
+        //for playlist menu
+
+        private void Create_playlist(object sender, EventArgs e) //makes new playlist
+        {
+            if (!File.Exists("C:/temp/playlists/new playlist.txt")) //prevents overwritting a pre existing "new playlist"
+            {
+                File.Create("C:/temp/playlists/new playlist.txt").Dispose();
+                Show_playlists();
+            }
+        }
+
+        private void Delete_playlist(object sender, EventArgs e) //removes selected playlist
+        {
+            File.Delete(Song_list[Song_box.SelectedIndex]);
+
+            Show_playlists();
+        }
+
+        private void Back_Click(object sender, EventArgs e) //goes back to playlist menu
+        {
+            if (Reader != null)
+            {
+                End_audio();
+            }
+            Show_playlists();
+
+            current_playlist = null;
+
+            Add_playlist.Enabled = true;
+            Rename_playlist.Enabled = true;
+            Remove_playlist.Enabled = true;
+            Back.Enabled = false;
+
+            GC.Collect();
+        }
+
+        private void Rename_Click(object sender, EventArgs e)//renames playlist
+        {
+            if (Song_box.SelectedIndex > -1)
+            {
+                try
+                {
+                    File.Move(Song_list[Song_box.SelectedIndex], "c:/temp/playlists/" + name_input.Text + ".txt");
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Name taken!");
+                }
+                name_input.Clear();
+
+                Show_playlists();
+            }
+        }
+
         //audio playback methods
 
         private void Start_audio(int index) //inits the reader and wave device
         {
-            Wave_out = new WaveOutEvent();
             Reader = new AudioFileReader(Song_list[index]);
             Wave_out.Init(Reader);
             Wave_out.Play();
             Timer.Start();
+
+            //inits timer and ui
             ttl_time.Text = Reader.TotalTime.ToString(@"mm\:ss");
             crnt_time.Text = Reader.CurrentTime.ToString(@"mm\:ss");
             seeker.Maximum = (int)Reader.TotalTime.TotalSeconds;
@@ -231,18 +291,15 @@ namespace music_player
 
             Timer.Stop();
 
-            //seems to fix the issue with NAudio becoming bloated
-            Wave_out = null;
             Reader = null;
         }
 
         private void Song_ended(object sender, EventArgs e) //autoplay
         {
-            if (Reader != null && Reader.Position == Reader.Length)
+            if (Reader != null && Reader.Position == Reader.Length) //sneaky way of determining if the song was ended by the user or not
             {
                 BeginInvoke(new Action(() =>
                 {
-                    Console.WriteLine("ok");
                     End_audio();
                     Current_index = Math.Clamp(Current_index + 1, 0, Song_list.Length);
                     Start_audio(Current_index);
@@ -254,8 +311,9 @@ namespace music_player
 
         //ui update methods
 
-        private void Update_playing()
+        private void Update_playing() //updates ui when song is changed
         {
+            //control
             TagLib.File Track = TagLib.File.Create(Song_list[Current_index]);
             crnt_Song.Text = Track.Tag.Title;
 
@@ -275,7 +333,7 @@ namespace music_player
 
         private void Update_time_seeker(object sender, System.Timers.ElapsedEventArgs e) //updates current time and seeker position
         {
-
+            //model + control
             BeginInvoke(new Action(() =>
             {
                 if (Reader != null)
@@ -289,22 +347,24 @@ namespace music_player
                 }
             }));
         }
-    
-        private void Show_playlists()
-        {
-            string[] playlists = Directory.GetFiles("C:/temp/playlists");
 
-            playlist_mode = true;
+        //for playlist menu
+
+        private void Show_playlists() //displays a list of available playlists
+        {
+            Song_list = Directory.GetFiles("C:/temp/playlists");
 
             Song_box.BeginUpdate();
 
             Song_box.Items.Clear();
-            foreach(string path in playlists)
+            foreach (string path in Song_list)
             {
-                Song_box.Items.Add(Path.GetFileName(path));
+                Song_box.Items.Add(Path.GetFileName(path.Substring(0, path.LastIndexOf(".txt"))));
             }
 
             Song_box.EndUpdate();
         }
+
+
     }
 }
